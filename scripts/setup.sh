0d2362c490f03ea06646210013a7473f2f00a27a
winky <y.kohada@gmail.com>
2023-11-21 15:46:50 +0900

    Fix: unuse code

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 if !(type "brew" > /dev/null 2>&1); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/winky/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/winky/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 if !(type "ansible" > /dev/null 2>&1); then
